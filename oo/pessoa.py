be000a02386320801ec43b4a4d670258f95c506c
Waldir Carlos <waldir.carlos.santos@gmail.com>
2020-12-14 15:14:32 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Carlos')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Waldir'
-    print(p.nome)
-    print(p.idade)
+    waldir = Pessoa(nome = 'Waldir')
+    antonio = Pessoa(waldir, nome = 'Antônio')
+    print(Pessoa.cumprimentar(antonio))
+    print(id(antonio))
+    print(antonio.cumprimentar())
+    print(antonio.nome)
+    print(antonio.idade)
+    for filho in antonio.filhos:
+        print(filho.nome)
