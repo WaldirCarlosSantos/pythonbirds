288b870ad45268922f69780c2af188edb6bd468b
Waldir Carlos <waldir.carlos.santos@gmail.com>
2020-12-19 06:27:00 -0300

    Implementado o Jogo PythonBirds

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -17,9 +17,17 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão.'
+
+class Mutante(Pessoa):
+    olhos = 4
+
 if __name__ == '__main__':
-    waldir = Pessoa(nome = 'Waldir')
-    antonio = Pessoa(waldir, nome = 'Antônio')
+    waldir = Mutante(nome = 'Waldir')
+    antonio = Homem(waldir, nome = 'Antônio')
     print(Pessoa.cumprimentar(antonio))
     print(id(antonio))
     print(antonio.cumprimentar())
@@ -40,3 +48,14 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(antonio), id(waldir))
     print(Pessoa.metodo_estatico(), antonio.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), antonio.nome_e_atributos_de_classe())
+
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(waldir, Pessoa))
+    print(isinstance(waldir, Homem))
+
+    print(waldir.olhos)
+
+    print(waldir.cumprimentar())
+    print(antonio.cumprimentar())
